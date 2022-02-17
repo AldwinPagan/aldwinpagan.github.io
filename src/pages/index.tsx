15a331c2fdc59e13e4aba4b76ae327f16e16abb1
AldwinPagan <aldwin.pagan@gmail.com>
2022-02-17 18:44:55 -0400

    [IMPLEMENT] Typeform Popover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,15 @@ import Testimonials from 'components/Testimonials';
 const IndexPage: React.FC = () => {
   return (
     <Layout>
+      <div
+        data-tf-popover="UyRPMhB5"
+        data-tf-button-color="#52B4E3"
+        data-tf-button-text="Launch me"
+        data-tf-iframe-props="title=Website Leads"
+        data-tf-medium="snippet"
+        style={{ all: 'unset' }}
+      ></div>
+      <script src="//embed.typeform.com/next/embed.js"></script>
       <SEO title="About Me" />
       <HeroBanner />
       <Services />
